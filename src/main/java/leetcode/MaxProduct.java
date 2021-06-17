9278f912372bd09dc68e322a1ff9537646a78b8a
kimjunho <wnsghi1@naver.com>
2021-06-17 22:00:12 +0900

    Leetcode > MaxProduct

---
+++ src/main/java/leetcode/MaxProduct.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class MaxProduct {
+    public static int maxProduct(int[] nums) {
+        Arrays.sort(nums);
+        return (nums[nums.length - 1] - 1) * (nums[nums.length - 2] - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProduct(new int[]{ 3,4,5,2 }));
+        System.out.println(maxProduct(new int[]{ 1,5,4,5 }));
+        System.out.println(maxProduct(new int[]{ 3,7 }));
+    }
+}
