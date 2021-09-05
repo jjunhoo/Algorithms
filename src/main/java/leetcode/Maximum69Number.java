f31c9d536a5736fb15b1e1d51bb3aafcb6489461
kimjunho <wnsghi1@naver.com>
2021-09-05 21:27:40 +0900

    leetcode > Maximum69Number

---
+++ src/main/java/leetcode/Maximum69Number.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+// Maximum69Number
+// https://leetcode.com/problems/maximum-69-number/
+public class Maximum69Number {
+    private static int maximum69Number(int num) {
+        char[] charArrNum = String.valueOf(num).toCharArray();
+
+        for (int i=0; i<charArrNum.length; i++) {
+            if (charArrNum[i] == '6') {
+                charArrNum[i] = '9';
+                break;
+            }
+        }
+
+        return Integer.parseInt(String.valueOf(charArrNum));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maximum69Number(9669));
+        System.out.println(maximum69Number(9996));
+        System.out.println(maximum69Number(9999));
+    }
+}
