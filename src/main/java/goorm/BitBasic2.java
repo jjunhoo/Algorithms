d56a1317dc7dd489a893d23b6d19217da846540e
kimjunho <wnsghi1@naver.com>
2021-09-08 00:42:20 +0900

    goorm > BitBasic2

---
+++ src/main/java/goorm/BitBasic2.java
@@ -0,0 +1,23 @@
+package goorm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// 비트연산 기본 2
+// https://level.goorm.io/exam/43173/%EB%B9%84%ED%8A%B8%EC%97%B0%EC%82%B0-%EA%B8%B0%EB%B3%B8-2/quiz/1
+public class BitBasic2 {
+    private static void solution(String input) {
+        String[] str = input.split(" ");
+        System.out.println("str : " + Arrays.toString(str));
+        int num1 = Integer.parseInt(str[0]);
+        int num2 = Integer.parseInt(str[1]);
+
+        System.out.println(num1 >> num2);
+        System.out.println(num1 << num2);
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        solution(br.readLine());
+    }
+}
