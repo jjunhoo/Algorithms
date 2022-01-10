5007db85a6d62a3aebae8e6e784a877f17b48e9e
kimjunho <wnsghi1@naver.com>
2022-01-10 23:13:00 +0900

    leetcode > Number of Steps to Reduce a Number to Zero

---
+++ src/main/java/leetcode/NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+// Number of Steps to Reduce a Number to Zero
+// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+public class NumberOfStepsToReduceANumberToZero {
+    private static int numberOfSteps(int num) {
+        int result = 0;
+
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            result++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("[result] : " + numberOfSteps(14)); // 6
+        System.out.println("[result] : " + numberOfSteps(8)); // 4
+        System.out.println("[result] : " + numberOfSteps(123)); // 12
+    }
+}
