101aa46bc3b2c1806f2d63f2fcec5e0edbebbf34
kimjunho <wnsghi1@naver.com>
2022-01-26 23:11:14 +0900

    leetcode > Search Insert Position

---
+++ src/main/java/leetcode/SearchInsertPosition.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+// Search Insert Position
+// https://leetcode.com/problems/search-insert-position/
+public class SearchInsertPosition {
+    private static int searchInsert(int[] nums, int target) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("[result] : " + searchInsert(new int[]{ 1,3,5,6 }, 5)); // 2
+        System.out.println("[result] : " + searchInsert(new int[]{ 1,3,5,6 }, 2)); // 1
+        System.out.println("[result] : " + searchInsert(new int[]{ 1,3,5,6 }, 7)); // 4
+    }
+}
