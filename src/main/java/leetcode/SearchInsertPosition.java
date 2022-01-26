8b11a7b4d3e4cf294b3a205f8afd1900298922e9
kimjunho <wnsghi1@naver.com>
2022-01-26 23:31:32 +0900

    leetcode > Search Insert Position

--- src/main/java/leetcode/SearchInsertPosition.java
+++ src/main/java/leetcode/SearchInsertPosition.java
@@ -1,11 +1,26 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 // Search Insert Position
 // https://leetcode.com/problems/search-insert-position/
 public class SearchInsertPosition {
     private static int searchInsert(int[] nums, int target) {
+        List<Integer> intList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        int result = 0;
 
-        return 0;
+        if (intList.contains(target)) {
+            return intList.indexOf(target);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) break;
+            result++;
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
